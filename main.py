f511ee581f71e25bd735ad82a3cf6baad87b4d67
VslVictor7 <vic.collfac358@outlook.com>
2024-12-07 19:01:17 -0300

    feat: pong command, inicial commit

---
+++ main.py
@@ -0,0 +1,25 @@
+import discord
+import os
+from discord.ext import commands
+from dotenv import load_dotenv
+
+load_dotenv()
+
+TOKEN = os.getenv("DISCORD_TOKEN")
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+bot = commands.Bot(command_prefix='!', intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f'Logged in as {bot.user.name} ({bot.user.id})')
+
+
+@bot.command()
+async def ping(ctx):
+    await ctx.send('Pong!')
+
+# Run the bot
+bot.run(TOKEN)
